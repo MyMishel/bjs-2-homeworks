ee0dc76a38c4ce88a36eb9d161a96a95a68fe15b
Кусливая Мишель <mishel.gorelik@mail.ru>
2022-06-16 22:07:43 +0500

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,25 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = {};
+  let x1, x2;
+
+  if (a === 0) {
+    return false;
+  }
+
+  let D = b ** 2 - 4 * a * c;
+  console.log("D = " + D);
+
+  if (D < 0) {
+    return "Корней нет";
+  } else if (D === 0) {
+    x1 = -b / (2 * a);
+    return x1;
+  } else if (D > 0) {
+    x1 = (-b + Math.sqrt(D)) / (2 * a);
+    x2 = (-b - Math.sqrt(D)) / (2 * a);
+    return "X1 = " + x1 + "; X2 = " + x2;
+  }
+  return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
-}
