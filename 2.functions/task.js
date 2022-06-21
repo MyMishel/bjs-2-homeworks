ad5d4bc4ca0b8e5f5426c9657374d6979c399b22
Кусливая Мишель <mishel.gorelik@mail.ru>
2022-06-22 00:01:06 +0500

    first commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,77 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  avg = parseFloat((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
+console.log(getArrayParams([-99, 99, 10]));//{ min: -99, max: 99, avg: 3.33 }
+
+
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
-
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sumOfElements = func(arrOfArr[i]);
+
+    if (sumOfElements > max) {
+      max = sumOfElements;
+    }
+  }
+
   return max;
 }
 
+console.log(makeWork([[1, 2, 3, 4], [10, 20, -10, -20]], worker))//10
+
+
+
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+
+  let minMeaning = Infinity;
+  let maxMeaning = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > maxMeaning) {
+      maxMeaning = arr[i];
+    }
+    if (arr[i] < minMeaning) {
+      minMeaning = arr[i];
+    }
+  }
+
+  resalt = parseFloat((maxMeaning - minMeaning).toFixed(2));
+
+  return resalt;
 }
+
+console.log(worker2([-10, -20, -40])); // 30
