5cbaf85edb9adcaa075bd3f4702eec43a622cb10
Кусливая Мишель <mishel.gorelik@mail.ru>
2022-06-29 00:15:00 +0500

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
+  result = arr1.every((item, i) => item === arr2[i] && arr1.length === arr2.length);
 
   return result; // boolean
 }
 
+
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
+  resultArr = arr.filter((item) => item > 0)
+    .filter((item) => item % 3 === 0)
+    .map((item) => item * 10);
 
   return resultArr; // array
 }
