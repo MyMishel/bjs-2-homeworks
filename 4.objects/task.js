1bd3b473966d6476c4858116c4ac4c8ed38edaba
Кусливая Мишель <mishel.gorelik@mail.ru>
2022-06-30 17:51:20 +0500

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,54 @@
+"use strict"
+
 function Student(name, gender, age) {
-    // Ваш код
-
+  //1
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
+let student1 = new Student("Антон", "мужчина", 27);
+let student2 = new Student("Артём", "мужчина", 30);
+let student3 = new Student("Галина", "женщина", 28);
+
+//2
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+//3
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+//4
+Student.prototype.addMarks = function (...rest) {
+  if (this.marks === undefined) {
+    this.marks = [...rest];
+  } else {
+    this.marks.push(...rest);
+  }
+}
+
+//5
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  let length = this.marks.length;
+
+  for (let i = 0; i < length; i++) {
+    sum += this.marks[i];
+  }
+  return sum / length;
+}
+
+//6
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;
+}
